d8624062f921d5594d1dac9e52e6a4f1414f272b
VISHNU <VISHNU>
2017-04-01 21:15:22 +0530

    Modified First Module Solution

--- Module1_Solution/js/app.js
+++ Module1_Solution/js/app.js
@@ -4,51 +4,50 @@ angular.module('LunchCheck',[])
 .controller('LunchCheckController',LunchCheckController);
 
 LunchCheckController.$inject = ['$scope'];
+
 function LunchCheckController($scope){
-
-// LunchCheckController.$inject = ['$scope','$filter'];
-
-// function LunchCheckController($scope,$filter){
   $scope.lunchItems="";
   $scope.checkItems = function(){
     var splitElem = $scope.lunchItems;
-    var elem = splitElem.split(",");
-    if(elem[0].length == 0 || elem[0] == ","){
-      elem.splice(0, 1);
-    }
-
-    var length = elem.length;
-    if(length>3){
-      $scope.lunchItemselem = "Too Much!!";
-      $scope.myObj = {
-          "border-color" : "red",
-      };
-      $scope.msgColor = {
-        "color" : "red",
-      }
-    }else if(length != 0){
-      $scope.lunchItemselem = "  Enjoy!";
-      $scope.myObj = {
-          "border-color" : "green",
-      };
-      $scope.msgColor = {
-        "color" : "green",
-      }
-    }else{
-      $scope.lunchItemselem = "Enter Items to Check !";
-      $scope.myObj = {
-          "border-color" : "black",
-      };
-      $scope.msgColor = {
-        "color" : "black",
-      }
-    }
-
-  //  alert();
+    var elem = elementSplitting(splitElem);
+    msgResponseSystem($scope,elem);
   }
-  //$scope.check = checkItems($scope,$filter);
-
 }
 
+function elementSplitting(splitElem){
+var elem = splitElem.split(",");
+  var arr = [];
+  angular.forEach(elem,function(value){
+    if(value.trim().length != 0){
+      arr.push(value);
+    }
+  });
+  return arr;
+}
+
+function msgResponseSystem( $scope, elem){
+  var length = elem.length;
+  if(length != 0){
+    if(length>3){
+      $scope.lunchItemselem = "Too Much!!";
+    }else{
+      $scope.lunchItemselem = "  Enjoy!";
+    }
+    $scope.myObj = {
+        "border-color" : "green",
+    };
+    $scope.msgColor = {
+      "color" : "green",
+    }
+  } else{
+    $scope.lunchItemselem = "Enter Items to Check !";
+    $scope.myObj = {
+        "border-color" : "red",
+    };
+    $scope.msgColor = {
+      "color" : "red",
+    }
+  }
+}
 
 })();
